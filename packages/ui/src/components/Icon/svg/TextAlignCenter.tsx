3528e9fb8df54a81dcf519d146a281c11c08464d
Sergey Tomashevsky <sergey.tomashevsky@gmail.com>
2026-03-29 22:34:49 +0200

    feat: add TextAlignCenter, TextAlignEnd icons; update TextAlignStart

---
+++ packages/ui/src/components/Icon/svg/TextAlignCenter.tsx
@@ -0,0 +1,11 @@
+import type { SVGProps } from "react"
+const TextAlignCenter = (props: SVGProps<SVGSVGElement>) => (
+  <svg width="1em" height="1em" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" {...props}>
+    <path
+      d="M19 18C19.5523 18 20 18.4477 20 19C20 19.5523 19.5523 20 19 20H5C4.44772 20 4 19.5523 4 19C4 18.4477 4.44772 18 5 18H19ZM17 11C17.5523 11 18 11.4477 18 12C18 12.5523 17.5523 13 17 13H7C6.44772 13 6 12.5523 6 12C6 11.4477 6.44772 11 7 11H17ZM21 4C21.5523 4 22 4.44772 22 5C22 5.55228 21.5523 6 21 6H3C2.44772 6 2 5.55228 2 5C2 4.44772 2.44772 4 3 4H21Z"
+      fill="currentColor"
+    />
+  </svg>
+)
+TextAlignCenter.displayName = "TextAlignCenter"
+export default TextAlignCenter
---
+++ packages/ui/src/components/Icon/svg/TextAlignEnd.tsx
@@ -0,0 +1,11 @@
+import type { SVGProps } from "react"
+const TextAlignEnd = (props: SVGProps<SVGSVGElement>) => (
+  <svg width="1em" height="1em" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" {...props}>
+    <path
+      d="M21 18C21.5523 18 22 18.4477 22 19C22 19.5523 21.5523 20 21 20H7C6.44772 20 6 19.5523 6 19C6 18.4477 6.44772 18 7 18H21ZM21 11C21.5523 11 22 11.4477 22 12C22 12.5523 21.5523 13 21 13H11C10.4477 13 10 12.5523 10 12C10 11.4477 10.4477 11 11 11H21ZM21 4C21.5523 4 22 4.44772 22 5C22 5.55228 21.5523 6 21 6H3C2.44772 6 2 5.55228 2 5C2 4.44772 2.44772 4 3 4H21Z"
+      fill="currentColor"
+    />
+  </svg>
+)
+TextAlignEnd.displayName = "TextAlignEnd"
+export default TextAlignEnd
